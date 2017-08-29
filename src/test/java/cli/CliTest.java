44cd81fb2dfd13381608df9805fc87f1de1a9e60
hecttact <hecttact@163.com>
2017-08-29 22:49:00 +0800

    commit

---
+++ src/test/java/cli/CliTest.java
@@ -0,0 +1,13 @@
+package cli;
+
+import org.apache.log4j.Logger;
+
+public class CliTest {
+
+	public final static Logger log = Logger.getLogger(CliTest.class);
+
+	public static void main(String[] args) {
+		log.info(2);
+	}
+
+}
