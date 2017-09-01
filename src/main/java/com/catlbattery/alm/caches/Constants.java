5d6fd647b7ff853c1728713ac9c9ae79f3fdbc4a
hecttact <hecttact@163.com>
2017-09-01 10:50:44 +0800

    m

--- src/main/java/com/catlbattery/alm/caches/Constants.java
+++ src/main/java/com/catlbattery/alm/caches/Constants.java
@@ -1,7 +1,6 @@
 package com.catlbattery.alm.caches;
 
 /**
- * 常量
  * 
  * @author
  *
@@ -11,52 +10,6 @@ public class Constants {
 	public static final String NEWLINE = System.getProperty("line.separator");
 
 	public static final String USER_HOME = "user.home";
-
-	/** * */
-	public static final String MKSSI_HOST = "MKSSI_HOST";
-
-	/** * */
-	public static final String MKSSI_USER = "MKSSI_USER";
-
-	/** * */
-	public static final String MKSSI_PORT = "MKSSI_PORT";
-
-	public static final String MKSSI_QUERYDEFINITION = "MKSSI_QUERYDEFINITION";
-
-	/**
-	 * * MKSSI_SORTASCENDING=true
-	 */
-	public static final String MKSSI_SORTASCENDING = "MKSSI_SORTASCENDING";
-
-	/**
-	 * * the sort field MKSSI_SORTFIELD=ID
-	 */
-	public static final String MKSSI_SORTFIELD = "MKSSI_SORTFIELD";
-
-	/**
-	 * * the issues of selected in current view MKSSI_NISSUE=5
-	 */
-	public static final String MKSSI_NISSUE = "MKSSI_NISSUE";
-
-	/**
-	 * * the issues ID of selected in current view MKSSI_ISSUE3=4444,
-	 * MKSSI_ISSUE4=4445, MKSSI_ISSUE1=4442
-	 */
-	public static final String MKSSI_ISSUE_X = "MKSSI_ISSUE%s";
-
-	/** * issues */
-	public static final String MKSSI_WINDOW = "MKSSI_WINDOW";
-
-	public static final String MKSSI_QUERY = "MKSSI_QUERY";
-
-	public static final String MKSSI_NFIELD = "MKSSI_NFIELD";
-
-	/**
-	 * * MKSSI_FIELD1=ID, MKSSI_FIELD2=Type, MKSSI_FIELD3=State
-	 */
-	public static final String MKSSI_FIELD_X = "MKSSI_FIELD%s";
-
-	public static final String MKSSI_FIELD_X_WIDTH = "MKSSI_FIELD%s_WIDTH";
 
 	public static final String USERDOMAIN = "USERDOMAIN";
 
@@ -102,40 +55,28 @@ public class Constants {
 
 	public static final String PHASE = "phase";
 
-	/** 父字段 **/
 	public static final String PARENT_FIELD = "Contained By";
 
-	/** 子字段 **/
 	public static final String SUB_FIELD = "Contains";
 
-	/** 关联字段 **/
 	public static final String REFERENCE_FIELD = "References";
 
-	/** 状态字段 **/
 	public static final String STATE_FIELD = "State";
 
-	/** 类型字段 **/
 	public static final String TYPE_FIELD = "Type";
 
-	/** 字段 **/
 	public static final String FIELDS = "fields";
 
-	/** test suite类型 **/
 	public static final String TYPE_TEST_SUIT = "Test Suit";
 
-	/** tc类型 **/
 	public static final String TYPE_TEST_CASE = "Test Case";
 
-	/** 操作类型 **/
 	public static final String INSERT_OPERATION = "Insert";
 
-	/** 富文本前缀 **/
 	public static final String RICH_CONTENT_PREFIX = "<!-- MKS HTML -->";
 
-	/** 富文本附件描述 **/
 	public static final String RICH_CONTENT_ATTACHMENT_SUMMARY = "Attachment added automatically by rich content";
 
-	/** 富文本mks路径 **/
 	public static final String RICH_CONTENT_MKS_SRC_PREFIX = "mks:///item/field?fieldid=%s&attachmentname=";
 
 }
